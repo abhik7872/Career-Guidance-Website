66ef2ea29595d21ed850f9632cfe1de87466c160
abhik7872 <chatterjeeabhik90@gmail.com>
2022-05-04 22:50:42 +0530

    File structure changed

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'landing.html'));
+});
 
 app.listen(3000);
