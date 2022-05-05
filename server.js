8ad725467a470fe8ff63092eceb6e72828b91bde
abhik7872 <chatterjeeabhik90@gmail.com>
2022-05-05 12:27:26 +0530

    Node server running

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'landing.html'));
+    res.sendFile(path.join(__dirname, 'public', 'landing.html'));
 });
 
 app.listen(3000);
